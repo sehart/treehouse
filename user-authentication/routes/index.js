acaa040e1830c3803fc22bd105f407d34d5de188
Stephen Hart <stephenehart@gmail.com>
2017-01-07 23:18:52 -0500

    sign in form working.

--- user-authentication/routes/index.js
+++ user-authentication/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 // GET /
 router.get('/', function(req, res, next) {
@@ -15,5 +16,47 @@ router.get('/about', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   return res.render('contact', { title: 'Contact' });
 });
+
+// GET /register
+router
+	.get('/register', function(req, res, next) {
+		return res.render('register', { title: 'Sign Up' });
+	})
+	.post('/register', function(req, res, next) {
+		if (req.body.email &&
+			req.body.name &&
+			req.body.favoriteBook &&
+			req.body.password &&
+			req.body.confirmPassword) {
+
+			//Make sure passwords match
+			if (req.body.password !== req.body.confirmPassword) {
+				var err = new Error('Passwords do not match.');
+				err.status = 400;
+				return next(err);
+			}
+
+			// Build user object model
+			var userData = {
+				email: req.body.email,
+				name: req.body.name,
+				favoriteBook: req.body.favoriteBook,
+				password: req.body.password
+			}
+
+			User.create(userData, function(error, user) {
+				if (error) {
+					return next(error);
+				} else {
+					return res.redirect('/profile');
+				}
+			});
+
+		} else {
+			var err = new Error("All fields required.");
+			err.status = 400;
+			return next(err);
+		}
+	});
 
 module.exports = router;
